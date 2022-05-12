77304cbd3a93def46e6526fb9930f8159d7da691
Milan <43573831+MilanSteman@users.noreply.github.com>
2022-05-12 12:32:36 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,30 @@
-// JavaScript Document
-console.log("Howdy!");
+const eeveeArray = ["eevee", "vaporeon", "jolteon", "flareon", "espeon", "umbreon", "glaceon", "leafeon"];
+const lastEevee = document.querySelector(".pad-links");
+const nextEevee = document.querySelector(".pad-rechts");
+const img = document.querySelector("section.scherm-links section");
+const currentNumb = 0;
 
+lastEevee.addEventListener("click", () => {
+	console.log(currentNumb);
+	if (currentNumb === 0) {
+		img.classList.remove(`${eeveeArray[currentNumb]}`)
+		currentNumb = eeveeArray.length - 1;
+		img.classList.add(`${eeveeArray[currentNumb]}`)
+	} else {
+		img.classList.remove(`${eeveeArray[currentNumb]}`)
+		currentNumb--;
+		img.classList.add(`${eeveeArray[currentNumb]}`)
+	}
+});
 
-var eeveeArray = ["eevee", "vaporeon", "jolteon", "flareon", "espeon", "umbreon", "glaceon", "leafeon"];
-
-var currentNumb = 0;
-
-var lastEevee = document.querySelector("button.pad-links");
-
-var nextEevee = document.querySelector("button.pad-rechts");
-var img = document.querySelector("section.scherm-links section");
-
-function naarLastEevee(){
-	console.log("test");
-	 if (currentNumb == 0) {
-	 	currentNumb = eeveeArray.length -1;
-	 	img.classList.add(`${eeveeArray[currentNumb]}`)
-	 }
-	 else{
-	 	currentNumb -1;
-	 	img.classList.add(`${eeveeArray[currentNumb]}`)
-	 }
-}
-
-lastEevee.addEventListener("click", naarLastEevee())
+nextEevee.addEventListener("click", () => {
+	if (currentNumb === eeveeArray.length - 1) {
+		img.classList.remove(`${eeveeArray[currentNumb]}`)
+		currentNumb = 0;
+		img.classList.add(`${eeveeArray[currentNumb]}`)
+	} else {
+		img.classList.remove(`${eeveeArray[currentNumb]}`)
+		currentNumb++;
+		img.classList.add(`${eeveeArray[currentNumb]}`)
+	}
+});
