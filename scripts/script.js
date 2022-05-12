765627831f599074c0fe6731193ee2b1ebe16cd8
YarreWille <61546394+YarreWille@users.noreply.github.com>
2022-05-12 12:21:22 +0200

    Add files via upload
    donderdag 12 mei

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,26 @@
 // JavaScript Document
 console.log("Howdy!");
+
+
+var eeveeArray = ["eevee", "vaporeon", "jolteon", "flareon", "espeon", "umbreon", "glaceon", "leafeon"];
+
+var currentNumb = 0;
+
+var lastEevee = document.querySelector("button.pad-links");
+
+var nextEevee = document.querySelector("button.pad-rechts");
+var img = document.querySelector("section.scherm-links section");
+
+function naarLastEevee(){
+	console.log("test");
+	 if (currentNumb == 0) {
+	 	currentNumb = eeveeArray.length -1;
+	 	img.classList.add(`${eeveeArray[currentNumb]}`)
+	 }
+	 else{
+	 	currentNumb -1;
+	 	img.classList.add(`${eeveeArray[currentNumb]}`)
+	 }
+}
+
+lastEevee.addEventListener("click", naarLastEevee())
